485906a7bbcf9cdd33b2d5f2c05bfb26e3f5d793
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-30 17:06:57 -0700

    added add new student functionality

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,44 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    html = render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github)
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/add_student")
+def get_student_add_form():
+    """Show form for adding a student."""
+
+    return render_template("new_student.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add student."""
+
+    #first_name, last_name, github = request.form.get('first_name, last_name, github')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("student-added.html",
+                           first_name=first_name,
+                           last_name=last_name,
+                           github=github)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
